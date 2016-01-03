a012c6a3fb0ade4c7131e325828a3d5f6b10eebd
blackmiaool <blackmiaool@gmail.com>
2016-01-03 21:33:32 +0800

    can use

--- dist/js/scenes/preload.js
+++ dist/js/scenes/preload.js
@@ -35,7 +35,7 @@ define(["sys", "angular", "v", "common", "res", "dbg"], regeneratorRuntime.mark(
                                 name = name[name.length - 2].split('/');
                                 name = name[name.length - 1];
                                 images[i] = new Image();
-                                images[i].src = res.img[i];
+                                images[i].src = "../../" + res.img[i];
                                 images[i].onload = function () {
                                     load_cnt++;
 
